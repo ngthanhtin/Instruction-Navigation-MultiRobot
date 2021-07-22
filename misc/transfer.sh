7621f464b8234579d1dcb769f0f8ee2f6ab2553f
nguyen_thanh_tin <ngthanhtinqn@gmail.com>
2021-07-22 14:43:23 +0900

    add misc

---
+++ misc/transfer.sh
@@ -0,0 +1,6 @@
+cp ./misc/launch/* ~/TIN/Robot_NLP_Code/rl-mapless-navigation/src/turtlebot3_simulations/turtlebot3_gazebo/launch/
+cp ./misc/worlds/* ~/TIN/Robot_NLP_Code/rl-mapless-navigation/src/turtlebot3_simulations/turtlebot3_gazebo/worlds/
+cp ./misc/turbot_urdf/* ~/TIN/Robot_NLP_Code/rl-mapless-navigation/src/turtlebot3/turtlebot3_description/urdf/
+cp -r ./misc/gazebo_models/* ~/.gazebo/models/
+cp -r ./misc/map_test_env1/ ~/
+cp -r ./misc/map_test_env2/ ~/
